0d52d06d87cd314fe94d1b3dfe542363c0c00f5c
astarta16 <ngedeshuri2001@gmail.com>
2022-10-09 11:06:55 -0700

    feat:Props and Custom Events

---
+++ src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+        <input type="text" placeholder="your name" v-model="enteredName" />
+        <input type="text" placeholder="your age" v-model="enteredAge" />
+        <button> Set User Data</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+          enteredName: "",
+          enteredAge: "",
+        };
+    },
+    methods: {
+        submitData(){
+             this.$emit('set-data', this.enteredName, this.enteredAge);
+        }
+       
+    }
+};
+</script>
